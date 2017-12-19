bfdf94b1286ec035e0c40b7d52d3957e4b088154
Michal Lyskawinski <mscteam99@gmail.com>
2017-12-19 12:42:13 +0100

    Extended ibm nlp features

--- models/ibm-nlp.py
+++ models/ibm-nlp.py
@@ -3,26 +3,32 @@ import os
 import json
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 \
-  import Features, EntitiesOptions, KeywordsOptions
+  import Features, EntitiesOptions, KeywordsOptions, RelationsOptions, ConceptsOptions
 
-# api keys 
+  # api keys 
 natural_language_understanding = NaturalLanguageUnderstandingV1(
   username= os.getenv('ibm_nlp_user'),
   password= os.getenv('ibm_nlp_pass'),
   version='2017-02-27')
 
-#
+class analyzer:
+  def emotions(self, text):
+    response = natural_language_understanding.analyze(
+      text=text,
+      features=Features(
+        entities=EntitiesOptions(
+          emotion=True,
+          sentiment=True,
+          limit=50),
+        keywords=KeywordsOptions(
+          emotion=True,
+          sentiment=True,
+          limit=50),
+        relations=RelationsOptions(),
+        concepts=ConceptsOptions()))
 
-response = natural_language_understanding.analyze(
-  text='ISO 639-1 code indicating the language to use for the analysis. This code overrides the automatic language detection performed by the service. Valid codes are ar (Arabic), en (English), fr (French), de (German), it (Italian), pt (Portuguese), ru (Russian), es (Spanish), and sv (Swedish). For more information about which features are supported in each language, se',
-  features=Features(
-    entities=EntitiesOptions(
-      emotion=True,
-      sentiment=True,
-      limit=2),
-    keywords=KeywordsOptions(
-      emotion=True,
-      sentiment=True,
-      limit=2)))
+    return(json.dumps(response, indent=2))
 
-print(json.dumps(response, indent=2))
+x = analyzer()
+sometxt = "Hey, man I had a bad day. I had to go to work and it sucked. The principal asked me to work on boring ass project. So boring that I wanted to kill myself."
+print(x.emotions(sometxt))
